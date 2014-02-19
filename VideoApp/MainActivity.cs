f0676ac90f724e492e89130810df134cc6cfaac4
Derick Beckwith <derick.beckwith@gmail.com>
2014-02-18 21:07:38 -0500

    Added methods GetOutputMediaFile and GetOutputMediaFileUri

--- VideoApp/MainActivity.cs
+++ VideoApp/MainActivity.cs
@@ -1,18 +1,27 @@
-﻿using System;
-
-using Android.App;
-using Android.Content;
-using Android.Hardware;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.OS;
-
-namespace VideoApp
+﻿namespace VideoApp
 {
+    using System;
+
+    using Android.App;
+    using Android.Hardware;
+    using Android.Net;
+    using Android.OS;
+    using Android.Util;
+    using Android.Widget;
+
+    using Java.IO;
+    using Java.Text;
+    using Java.Util;
+
+    using Uri = Android.Net.Uri;
+
     [Activity(Label = "VideoApp", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private const int MediaTypeImage = 1;
+
+        private const int MediaTypeVideo = 2;
+
         private Camera camera;
 
         private CameraPreview cameraPreview;
@@ -93,6 +102,61 @@ namespace VideoApp
             base.OnPause();
 
             this.ReleaseCamera();
+        }
+
+        /// <summary>
+        /// Creates a file Uri for saving an image or video.
+        /// </summary>
+        /// <param name="type">The media file type.</param>
+        /// <returns>An instance of Uri for saving a media file to.</returns>
+        private static Uri GetOutputMediaFileUri(int type)
+        {
+            return Uri.FromFile(GetOutputMediaFile(type));
+        }
+
+        /// <summary>
+        /// Creates a File for saving an image or video.
+        /// </summary>
+        /// <param name="type">The media file type to be created.</param>
+        /// <returns>An instance of File to use for saving media to.</returns>
+        private static File GetOutputMediaFile(int type)
+        {
+            // TODO: Check that the SDCard is mounted using Environment.GetExternalStorageState() before doing this.
+            // This location works best if you want the created images to be shared
+            // between applications and persist after your app has been uninstalled.
+            File mediaStorageDir = new File(
+                Android.OS.Environment.GetExternalStoragePublicDirectory(
+                Android.OS.Environment.DirectoryPictures), 
+                "MyCameraApp");
+
+            // Create the storage directory if it does not exist.
+            if (!mediaStorageDir.Exists())
+            {
+                if (!mediaStorageDir.Mkdirs())
+                {
+                    Log.Debug("MyCameraApp", "failed to create directory");
+                    return null;
+                }
+            }
+
+            // Create a media file name.
+            string timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").Format(new Date());
+            File mediaFile;
+
+            if (type == MediaTypeImage)
+            {
+                mediaFile = new File(mediaStorageDir.Path + File.Separator + "IMG_" + timeStamp + ".jpg");
+            }
+            else if (type == MediaTypeVideo)
+            {
+                mediaFile = new File(mediaStorageDir.Path + File.Separator + "VID_" + timeStamp + ".mp4");
+            }
+            else
+            {
+                return null;
+            }
+
+            return mediaFile;
         }
 
         private void ReleaseCamera()
